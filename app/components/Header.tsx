0d2f5c2c05e9976066b7a31ec7cc89b80f602920
Nestor <zozomezo@abv.bg>
2024-11-16 14:18:09 +0200

    Bring back logo

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,6 +3,14 @@ export default function Header() {
         <header>
             <nav className="bg-white border-gray-200 dark:bg-gray-900">
                 <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                    <a
+                        href="/"
+                        className="flex items-center space-x-3 rtl:space-x-reverse"
+                    >
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                            Sample Logo
+                        </span>
+                    </a>
                     <button
                         data-collapse-toggle="navbar-default"
                         type="button"
