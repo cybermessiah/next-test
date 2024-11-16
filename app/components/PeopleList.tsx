f08b46b59f98662daf816df3da7ce54e95e57606
Nestor <zozomezo@abv.bg>
2024-11-16 15:35:44 +0200

    Adjust styling

--- app/components/PeopleList.tsx
+++ app/components/PeopleList.tsx
@@ -18,28 +18,29 @@ async function loadPeople(): Promise<IPeople[]> {
 const PeopleList = async () => {
     const persons = await loadPeople()
     return (
-        <div className="lg:px-5 lg:py-12">
-            <h3 className="text-3xl uppercase font-extrabold text-white max-w-[320px] pl-2 py-4 lg:py-0 m-auto lg:m-0">
+        <>
+            <h3 className="text-3xl uppercase font-bold text-center">
                 People List
             </h3>
-
-            {persons.map((individual) => (
-                <div
-                    key={individual.id}
-                    className={`flex flex-row justify-between items-end max-w-sm rounded overflow-hidden shadow-lg border border-solid border-white my-4`}
-                >
-                    <div className="px-6 py-4">
-                        <h3>{individual.name}</h3>
-                        <p>{individual.bio}</p>
+            <div className="lg:px-5 lg:py-12">
+                {persons.map((individual) => (
+                    <div
+                        key={individual.id}
+                        className="flex flex-row justify-between items-end rounded overflow-hidden shadow-lg border border-solid border-white my-4"
+                    >
+                        <div className="px-6 py-4">
+                            <h3 className="text-2xl">{individual.name}</h3>
+                            <p>{individual.bio}</p>
+                        </div>
+                        <div className="px-6 pt-4 pb-2">
+                            <span className="inline-block rounded-full px-3 text-4xl font-semibold mr-2 mb-4">
+                                {individual.version}
+                            </span>
+                        </div>
                     </div>
-                    <div className="px-6 pt-4 pb-2">
-                        <span className="inline-block rounded-full px-3 text-4xl font-semibold mr-2 mb-4">
-                            {individual.version}
-                        </span>
-                    </div>
-                </div>
-            ))}
-        </div>
+                ))}
+            </div>
+        </>
     )
 }
 
