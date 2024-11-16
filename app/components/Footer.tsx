b6208791d17f6bda64ad577a4f938d8dd88eb6c2
Nestor <zozomezo@abv.bg>
2024-11-16 14:09:20 +0200

    Add footer

---
+++ app/components/Footer.tsx
@@ -0,0 +1,118 @@
+export default function Footer() {
+    return (
+        <footer className="max-w-7xl m-auto p-8">
+            <div className="sm:flex mb-4 justify-center gap-2">
+                <div className="sm:w-1/4 h-auto">
+                    <div className="uppercase font-extrabold mb-2">
+                        <h6>Product Main</h6>
+                    </div>
+                    <ul className="list-reset leading-normal">
+                        <li className="hover:text-orange text-grey-darker">
+                            Sample product
+                        </li>
+                        <li className="hover:text-orange text-grey-darker">
+                            Sample product
+                        </li>
+                        <li className="hover:text-orange text-grey-darker">
+                            Sample product
+                        </li>
+                        <li className="hover:text-orange text-grey-darker">
+                            Four
+                        </li>
+                        <li className="hover:text-orange text-grey-darker">
+                            Five
+                        </li>
+                        <li className="hover:text-orange text-grey-darker">
+                            Six
+                        </li>
+                        <li className="hover:text-blue text-grey-darker">
+                            Legal
+                        </li>
+                    </ul>
+                </div>
+                <div className="sm:w-1/4 h-auto sm:mt-0 mt-8">
+                    <ul className="list-reset leading-normal mt-8">
+                        <li className="hover:text-blue text-grey-darker">
+                            Sample product
+                        </li>
+                        <li className="hover:text-blue text-grey-darker">
+                            Sample product
+                        </li>
+                        <li className="hover:text-blue text-grey-darker">
+                            Three
+                        </li>
+                        <li className="hover:text-blue text-grey-darker">
+                            One
+                        </li>
+                        <li className="hover:text-blue text-grey-darker">
+                            Sample product
+                        </li>
+                        <li className="hover:text-blue text-grey-darker">
+                            Sample product
+                        </li>
+                        <li className="hover:text-blue text-grey-darker">
+                            Sample product
+                        </li>
+                    </ul>
+                </div>
+                <div className="sm:w-1/4 h-auto sm:mt-0 mt-8">
+                    <div className="text-green-dark mb-2">
+                        <div className="uppercase font-extrabold mb-2">
+                            <h6>The legal stuff</h6>
+                        </div>
+                    </div>
+                    <ul className="list-reset leading-normal">
+                        <li className="hover:text-green-dark text-grey-darker">
+                            One
+                        </li>
+                        <li className="hover:text-green-dark text-grey-darker">
+                            Two
+                        </li>
+                        <li className="hover:text-green-dark text-grey-darker">
+                            Three
+                        </li>
+                        <li className="hover:text-green-light text-grey-darker">
+                            One
+                        </li>
+                        <li className="hover:text-green-light text-grey-darker">
+                            Two
+                        </li>
+                        <li className="hover:text-green-light text-grey-darker">
+                            Three
+                        </li>
+                    </ul>
+                </div>
+                <div className="sm:w-1/4 sm:mt-0 mt-8 h-auto">
+                    <div className="text-red-light mb-2">
+                        <div className="uppercase font-extrabold mb-2">
+                            <h6>Company Info</h6>
+                        </div>
+                    </div>
+                    <ul className="list-reset leading-normal">
+                        <li className="hover:text-green-dark text-grey-darker">
+                            Company
+                        </li>
+                        <li className="hover:text-green-dark text-grey-darker">
+                            Press Releases
+                        </li>
+                        <li className="hover:text-green-dark text-grey-darker">
+                            Three
+                        </li>
+                        <li className="hover:text-green-light text-grey-darker">
+                            One
+                        </li>
+                        <li className="hover:text-green-light text-grey-darker">
+                            Two
+                        </li>
+                        <li className="hover:text-green-light text-grey-darker">
+                            Three
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div className="flex justify-between border-t-2 border-black-main py-2 flex-wrap">
+                <span>© 2024 Sample Company</span>
+            </div>
+        </footer>
+    )
+}
