ae814e282d935416333daa8b8e33718a950f919f
Nestor <zozomezo@abv.bg>
2024-11-16 15:13:03 +0200

    Add mock json data and display

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -19,21 +19,6 @@ export default function Header() {
                         aria-expanded="false"
                     >
                         <span className="sr-only">Open main menu</span>
-                        <svg
-                            className="w-5 h-5"
-                            aria-hidden="true"
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 17 14"
-                        >
-                            <path
-                                stroke="currentColor"
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
-                                stroke-width="2"
-                                d="M1 1h15M1 7h15M1 13h15"
-                            />
-                        </svg>
                     </button>
                     <div
                         className="hidden w-full md:block md:w-auto"
@@ -63,14 +48,6 @@ export default function Header() {
                                     className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                                 >
                                     Services
-                                </a>
-                            </li>
-                            <li>
-                                <a
-                                    href="#"
-                                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                                >
-                                    Pricing
                                 </a>
                             </li>
                             <li>
---
+++ app/components/ProductList.tsx
@@ -0,0 +1,33 @@
+import fsPromises from 'fs/promises';
+import path from 'path'
+
+interface IPeople {
+	id: string;
+	name: string;
+	bio: string;
+	version: number;
+}
+
+async function loadPeople(): Promise<IPeople[]> {
+    const filePath = path.join(process.cwd(), 'lib/data.json');
+    const jsonData = await fsPromises.readFile(filePath);
+    const dataToDisplay:IPeople[] = JSON.parse(jsonData.toString());
+  return dataToDisplay;
+}
+
+const PostList = async () => {
+  const persons = await loadPeople();
+  return (
+    <div className="post-list">
+      {persons.map((individual) => (
+        <div key={individual.id} className="individual-listing">
+          <h3 className="individual-title">{individual.name}</h3>
+          <p className="individual-body">{individual.bio}</p>
+          <span className="individual-body">{individual.version}</span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PostList;
